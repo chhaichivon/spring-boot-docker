c373346a2ffc5715c1ae718e16f9648d089b3b8a
chhaichivon <chhaivinchivon@gmail.com>
2018-03-11 17:13:14 +0700

    update pom

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 **SPRING BOOT DOCKER**
+
+
+
+
+Build
+
+`**mvnw install dockerfile:build**`
+
+
+
+
+
+Reference 
+
+`https://spring.io/guides/gs/spring-boot-docker/
+http://www.baeldung.com/dockerizing-spring-boot-application`
---
+++ src/main/java/com/chhaichivon/application/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.chhaichivon.application.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+
+/**
+ * Auhtor : chhaichivon on 3/11/18
+ */
+@Controller
+public class HomeController {
+
+    public String index(){
+        return  "Home";
+    }
+}
